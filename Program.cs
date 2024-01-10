f4979fba36068789bed12d91d5c5d6677f9ea9e2
Henrysoto <rtcwpub@gmail.com>
2024-01-10 14:51:46 +0100

    day 1 solved

--- Program.cs
+++ Program.cs
@@ -14,9 +14,6 @@ public class Program
         {
             Dictionary<int, int> values = new Dictionary<int, int>();
 
-            //8sevenkrpqkfxdcn7eightfour7lfpbtztlmfg => wrong: 84 => answer: 87 
-            //Debug: [0: 8],[1: 7],[16: 7],[17: 8],[22: 4]
-
             char[] chrs = line.ToCharArray();
             for (var i = 0; i < aoc_utils.validnum.Length; i++)
             {
@@ -44,28 +41,12 @@ public class Program
                     }
                 }
             }
-            //foreach (var word in aoc_utils.validstr.Select((value, i) => new { i, value }))
-            //{
-            //    if (line.Contains(word.value))
-            //    {
-            //        int idx = line.IndexOf(aoc_utils.validstr[word.i]);
-            //        if (values.ContainsKey(idx))
-            //        {
-            //            idx = line.IndexOf(aoc_utils.validstr[word.i], idx + 1);
-            //            Console.WriteLine($"index validstr: {idx}");
-            //        }
-            //        if (idx > -1)
-            //        {
-            //            values.Add(idx, word.i);
-            //        }
-            //    }
-            //}
 
             SortedDictionary<int, int> output = new SortedDictionary<int, int>(values);
             int num = 0;
             if (output.Count == 1)
             {
-                num = Convert.ToInt32($"{output.First().Value}");
+                num = Convert.ToInt32($"{output.First().Value}{output.First().Value}");
             }
             else
             {
