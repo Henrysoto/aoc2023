c22e60cf6c61e3213233536559531cc94b30e5ee
Henrysoto <rtcwpub@gmail.com>
2024-01-12 18:38:35 +0100

    day3p1 wip

--- Program.cs
+++ Program.cs
@@ -8,14 +8,19 @@
             string path = $"{Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName}\\Inputs\\";
 
             // Day 1
-            input = File.ReadAllLines($"{path}day1.txt");
-            Console.WriteLine($"Day 1 part 1 solution: {AdventOfCode_2023.Solutions.Day1.part1_solution(input)}");
-            Console.WriteLine($"Day 1 part 2 solution: {AdventOfCode_2023.Solutions.Day1.part2_solution(input)}\n");
+            //input = File.ReadAllLines($"{path}day1.txt");
+            //Console.WriteLine($"Day 1 part 1 solution: {AdventOfCode_2023.Solutions.Day1.part1_solution(input)}");
+            //Console.WriteLine($"Day 1 part 2 solution: {AdventOfCode_2023.Solutions.Day1.part2_solution(input)}\n");
 
             // Day 2
-            input = File.ReadAllLines($"{path}day2.txt");
-            Console.WriteLine($"Day 2 part 1 solution: {AdventOfCode_2023.Solutions.Day2.part1_solution(input)}");
-            Console.WriteLine($"Day 2 part 2 solution: {AdventOfCode_2023.Solutions.Day2.part2_solution(input)}\n");
+            //input = File.ReadAllLines($"{path}day2.txt");
+            //Console.WriteLine($"Day 2 part 1 solution: {AdventOfCode_2023.Solutions.Day2.part1_solution(input)}");
+            //Console.WriteLine($"Day 2 part 2 solution: {AdventOfCode_2023.Solutions.Day2.part2_solution(input)}\n");
+
+            // Day 3
+            input = File.ReadAllLines($"{path}day3.txt");
+            Console.WriteLine($"Day 3 part 1 solution: {AdventOfCode_2023.Solutions.Day3.part1_solution(input)}");
+
         }
     }
 }
---
+++ Solutions/Day3.cs
@@ -0,0 +1,112 @@
+﻿namespace AdventOfCode_2023.Solutions
+{
+    internal class Day3
+    {
+        public static int part1_solution(string[] input)
+        {
+            int result = 0;
+
+            // Chaque ligne
+            for (uint i = 0; i < input.Length; i++)
+            {
+                List<char> chrs = new List<char>();
+                bool dot = true;
+
+                // Chaque caractere
+                for (int k = 0; k < input[i].Length; k++)
+                {
+                    char c = input[i][k];
+                    if (c != '.')
+                    {
+                        if (char.IsAsciiDigit(c))
+                        {
+                            dot = false;
+                            chrs.Add(c);
+                        }
+                    }
+                    else
+                    {
+                        if (!dot)
+                        {
+                            chrs.Add(',');
+                        }
+                        dot = true;
+                    }
+                }
+
+                // recherche de symboles adjacent
+                string str = new string(chrs.ToArray());
+                int lastIndex = 0;
+                foreach (var num in str.Split(','))
+                {
+                    if (!string.IsNullOrEmpty(num.Trim()))
+                    {
+                        bool symbolFound = false;
+                        int index = input[i].IndexOf(num, lastIndex);
+                        int start = 0;
+                        int end = 0;
+                        if (index > 0)
+                            start = index - 1;
+                        if ((index + num.Length - 1) <= input[i].Length)
+                            end = index + num.Length;
+                        else
+                            break;
+                        if (index == -1)
+                            break;
+                        else
+                            lastIndex = index;
+                        //Console.WriteLine($"\n\nLine: {i}\nNeedle: {num}\nstart: {start} end: {end}");
+
+                        // Current line
+                        for (var j = start; j <= end; j++)
+                        {
+                            if (j > input[i].Length - 1) { break; }
+                            char c = input[i][j];
+                            if (!char.IsDigit(c) && c != '.')
+                            {
+                                result += Convert.ToInt32(num);
+                                Console.WriteLine($"Line {i + 1} with needle {num} found symbol in: [{input[i][j]}] at index 0/{j} \t\t- Result: {result}");
+                                symbolFound = true;
+                                break;
+                            }
+                        }
+                        // Up
+                        if (i > 0)
+                        {
+                            for (var j = start; j <= end; j++)
+                            {
+                                if (j > input[i].Length - 1) { break; }
+                                char c = input[i - 1][j];
+                                if (!char.IsDigit(c) && c != '.')
+                                {
+                                    result += Convert.ToInt32(num);
+                                    Console.WriteLine($"Line {i + 1} with needle {num} found symbol up: [{input[i - 1][j]}] at index -1/{j} \t\t- Result: {result}");
+                                    symbolFound = true;
+                                    break;
+                                }
+                            }
+                        }
+                        // Down
+                        if (i < input.Length - 1 && !symbolFound)
+                        {
+                            for (var j = start; j <= end; j++)
+                            {
+                                if (j > input[i].Length - 1) { break; }
+                                char c = input[i + 1][j];
+                                if (!char.IsDigit(c) && c != '.')
+                                {
+                                    result += Convert.ToInt32(num);
+                                    Console.WriteLine($"Line {i + 1} with needle {num} found symbol dw: [{input[i+1][j]}] at index +1/{j} \t\t- Result: {result}");
+                                    symbolFound = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }      
+                }
+            }
+            
+            return result;
+        }
+    }
+}
